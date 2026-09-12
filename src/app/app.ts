a253bed381d99f6cd4b2f650ee4e5c43100a3e2c
MTS775 <sallatalla12@gmail.com>
2026-09-12 22:04:45 +0000

    Mise en place de la squelette du projet

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,9 +5,6 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  templateUrl: './app.html',
 })
-export class AppComponent {
-  title = 'gestion-notes-frontend';
-}
+export class App {}
