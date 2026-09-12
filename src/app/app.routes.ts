9601f3bac72f12527c18ece90072cd1b6020dab6
MTS775 <sallatalla12@gmail.com>
2026-09-12 22:50:12 +0000

    Mise en place de l'interface d'inscription

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { InscriptionFormComponent } from './features/inscription/components/inscription-form/inscription-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'inscription', component: InscriptionFormComponent },
+  { path: '', redirectTo: '/inscription', pathMatch: 'full' },
+  { path: '**', redirectTo: '/inscription' }
+];
