1404b14c44a9c1ded212cf404f8871eb8ee13569
MTS775 <sallatalla12@gmail.com>
2026-09-12 23:07:24 +0000

    Mise en place de l'interface de connexion

---
+++ src/app/features/connexion/components/connexion-form/connexion-form.component.ts
@@ -0,0 +1,53 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ConnexionRequest } from '../../models/connexion.model';
+
+@Component({
+  selector: 'app-connexion-form',
+  standalone: true,
+  imports: [CommonModule, RouterModule, ReactiveFormsModule],
+  templateUrl: './connexion-form.component.html',
+  styleUrl: './connexion-form.component.scss'
+})
+export class ConnexionFormComponent {
+  loginForm: FormGroup;
+  isSubmitting = false;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.invalid) {
+      this.markFormGroupTouched(this.loginForm);
+      return;
+    }
+
+    this.isSubmitting = true;
+    const connexionData: ConnexionRequest = this.loginForm.value;
+
+    // TODO: Appeler le service de connexion
+    console.log('Données de connexion:', connexionData);
+
+    setTimeout(() => {
+      this.isSubmitting = false;
+      this.loginForm.reset();
+      alert('Connexion réussie !');
+    }, 1000);
+  }
+
+  private markFormGroupTouched(formGroup: FormGroup): void {
+    Object.keys(formGroup.controls).forEach(key => {
+      const control = formGroup.get(key);
+      control?.markAsTouched();
+    });
+  }
+
+  get email() { return this.loginForm.get('email'); }
+  get password() { return this.loginForm.get('password'); }
+}
