e8bbb26232c54f25030289945bb471b0570b9ef5
MTS775 <sallatalla12@gmail.com>
2026-09-12 23:22:36 +0000

    Mise en place de l'interface du mot de passe oublie

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { InscriptionFormComponent } from './features/inscription/components/inscription-form/inscription-form.component';
 import { ConnexionFormComponent } from './features/connexion/components/connexion-form/connexion-form.component';
+import { ForgotPasswordFormComponent } from './features/forgot-password/components/forgot-password-form/forgot-password-form.component';
 
 export const routes: Routes = [
   { path: 'login', component: ConnexionFormComponent },
   { path: 'register', component: InscriptionFormComponent },
+  { path: 'forgot-password', component: ForgotPasswordFormComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: '**', redirectTo: '/login' }
+  { path: '**', redirectTo: '/login' },
 ];
